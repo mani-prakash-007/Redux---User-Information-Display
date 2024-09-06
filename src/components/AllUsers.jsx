5c460a070b8d192f23a03df5495e64edda180ea7
mani-prakash-007 <manip8072@gmail.com>
2024-09-06 06:57:09 +0530

    Added Srarch Functionality to the UserInformation Display

--- src/components/AllUsers.jsx
+++ src/components/AllUsers.jsx
@@ -6,6 +6,10 @@ import { fetchUsers } from "../Redux/Slices/userSlice";
 import { FaUserPlus } from "react-icons/fa6";
 
 export const AllUsers = () => {
+  //State
+  const [allUsersList, setAllUsersList] = useState();
+  const [usersList, setUsersList] = useState();
+
   //Redux Store Access
   const { users, loading, error } = useSelector(
     (state) => state.userInformation
@@ -18,7 +22,27 @@ export const AllUsers = () => {
     if (users.length === 0) {
       dispatch(fetchUsers());
     }
-  }, [dispatch]);
+    setUsersList(users);
+    setAllUsersList(users);
+  }, [dispatch, users]);
+
+  console.log("User States", usersList);
+  console.log("All Users State", allUsersList);
+  //Search functionality
+  const search = (e) => {
+    console.log(e.target.value);
+    const data = usersList.filter((data) =>
+      data.name.startsWith(e.target.value)
+    );
+    if (e.target.value.length === 0) {
+      setUsersList(allUsersList);
+    } else if (data.length > 0) {
+      console.log(data);
+      setUsersList(data);
+    } else if (data.length === 0) {
+      setUsersList("No Users Found");
+    }
+  };
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error}</p>;
@@ -30,13 +54,19 @@ export const AllUsers = () => {
           All User Information
         </h1>
         <div className="flex flex-col justify-center items-center my-10">
-          <div className="flex justify-end relative overflow-x-auto sm:rounded-lg w-1/3 my-5">
+          <div className="flex justify-end flex-wrap relative overflow-x-auto sm:rounded-lg w-1/3 my-5">
             <NavLink
               to={"/users/adduser"}
               className="border border-gray-300 px-5 py-2 mr-5  rounded-lg text-red-600 hover:text-white hover:bg-red-600 active:scale-90"
             >
               <FaUserPlus />
             </NavLink>
+            <input
+              onChange={search}
+              type="text"
+              className="border rounded-lg px-5 w-40"
+              placeholder="Search for user"
+            />
           </div>
           <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-1/3 border border-gray-300">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -57,31 +87,37 @@ export const AllUsers = () => {
                 </tr>
               </thead>
               <tbody>
-                {users.map((userData, index) => {
-                  return (
-                    <tr
-                      key={index}
-                      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-                    >
-                      <th
-                        scope="row"
-                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                {usersList === "No Users Found" ? (
+                  <>
+                    <p className="my-5">No users Found</p>
+                  </>
+                ) : (
+                  usersList?.map((userData, index) => {
+                    return (
+                      <tr
+                        key={index}
+                        className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
                       >
-                        {userData.id}
-                      </th>
-                      <td className="px-6 py-4">{userData.name}</td>
-                      <td className="px-6 py-4">{userData.email}</td>
-                      <td className="px-6 py-4 text-right">
-                        <Link
-                          to={`/users/${userData.id}`}
-                          className="font-medium p-2 rounded-xl text-blue-600 hover:bg-blue-600 hover:text-white transition-colors duration-300"
+                        <th
+                          scope="row"
+                          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                         >
-                          View Details
-                        </Link>
-                      </td>
-                    </tr>
-                  );
-                })}
+                          {userData.id}
+                        </th>
+                        <td className="px-6 py-4">{userData.name}</td>
+                        <td className="px-6 py-4">{userData.email}</td>
+                        <td className="px-6 py-4 text-right">
+                          <Link
+                            to={`/users/${userData.id}`}
+                            className="font-medium p-2 rounded-xl text-blue-600 hover:bg-blue-600 hover:text-white transition-colors duration-300"
+                          >
+                            View Details
+                          </Link>
+                        </td>
+                      </tr>
+                    );
+                  })
+                )}
               </tbody>
             </table>
           </div>
---
+++ src/components/Example.js
@@ -0,0 +1,18 @@
+const object = {
+  Name: "Mani Prakash",
+  Address: [
+    {
+      street: "ABC Street",
+      city: "CBE",
+      pincode: "686358",
+    },
+    {
+      street: "zcf Street",
+      city: "fhdtm",
+      pincode: "686358",
+    },
+  ],
+};
+
+console.log(typeof object);
+console.log(object.Address[1].street);
